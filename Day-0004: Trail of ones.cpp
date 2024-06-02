5cda6803511a24398aa738b62d0a3325e35dc9ec
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-06-03 05:24:05 +0530

    Create Day-0004: Trail of ones.cpp

---
+++ Day-0004: Trail of ones.cpp
@@ -0,0 +1,31 @@
+// Trail of ones (EASY)
+// https://www.geeksforgeeks.org/problems/trail-of-ones3242/1
+
+class Solution {
+  public:
+  
+    int mod=1e9+7;
+    long long power(long long a,long long b){
+        long long ans=1;
+        while(b){
+            if(b%2)ans*=a;
+            b>>=1;
+            a*=a;
+            a%=mod;
+            ans%=mod;
+        }
+        return ans;
+    }
+    
+    long long solve(int n,vector<int> &dp){
+        if(n==2) return 1;
+        if(n==3) return 3;
+        if(dp[n]!=-1)return dp[n];
+        return dp[n]=((solve(n-1,dp) + pow(2,n-2))%mod+ solve(n-2,dp))%mod;
+    }
+    
+    int numberOfConsecutiveOnes(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
