bd0004696356e58fd0ee7c7a5c767143ddc576af
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-07-14 10:55:27 +0530

    Create Day-0046: Segregate 0s and 1s.cpp

---
+++ Day-0046: Segregate 0s and 1s.cpp
@@ -0,0 +1,10 @@
+// Segregate 0s and 1s (EASY)
+// https://www.geeksforgeeks.org/problems/segregate-0s-and-1s5106/1
+
+
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        sort(arr.begin(), arr.end());
+    }
+};
