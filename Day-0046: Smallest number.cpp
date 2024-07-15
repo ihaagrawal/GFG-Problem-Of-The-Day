a28f34710c6d57203b305742756cb6d785ab9954
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-07-15 09:12:36 +0530

    Create Day-0046: Smallest number.cpp

---
+++ Day-0046: Smallest number.cpp
@@ -0,0 +1,26 @@
+// Smallest number (MEDIUM)
+// https://www.geeksforgeeks.org/problems/smallest-number5829/1
+
+
+class Solution {
+  public:
+    string smallestNumber(int s, int d) {
+       if(s > 9*d) return "-1";
+       
+       string result(d, '0');
+       s--;
+       
+       for(int i=d-1;i>0;i--){
+           if(s > 9){
+               result[i] = '9';
+               s -= 9;
+           }else{
+               result[i] = '0' + s;
+               s = 0;
+           }
+       }
+       
+       result[0] = '1' + s;
+       return result;
+    }
+};
