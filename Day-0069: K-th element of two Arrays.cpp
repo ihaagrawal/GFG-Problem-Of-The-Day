b0cac8ca31c772e68112b9765589b3320e7a83b2
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-08-07 23:25:41 +0530

    Create Day-0069: K-th element of two Arrays.cpp

---
+++ Day-0069: K-th element of two Arrays.cpp
@@ -0,0 +1,13 @@
+// K-th element of two Arrays (MEDIUM)
+// https://www.geeksforgeeks.org/problems/k-th-element-of-two-sorted-array1317/1
+
+class Solution {
+  public:
+    int kthElement(int k, vector<int>& arr1, vector<int>& arr2) {
+        vector<int> result;
+        result.insert(result.end(), arr1.begin(), arr1.end());
+        result.insert(result.end(), arr2.begin(), arr2.end());
+        sort(result.begin(), result.end());
+        return result[k-1];
+    }
+};
