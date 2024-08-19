a1b9102c18c89556024927fcdb1a23a36fb24ce3
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-08-19 10:18:52 +0530

    Create Day-0081: Kth Smallest.cpp

---
+++ Day-0081: Kth Smallest.cpp
@@ -0,0 +1,13 @@
+// Kth Smallest (MEDIUM)
+// https://www.geeksforgeeks.org/problems/kth-smallest-element5635/1
+
+
+class Solution {
+  public:
+    // arr : given array
+    // k : find kth smallest element and return using this function
+    int kthSmallest(vector<int> &arr, int k) {
+        sort(arr.begin(), arr.end());
+        return arr[k-1];
+    }
+};
