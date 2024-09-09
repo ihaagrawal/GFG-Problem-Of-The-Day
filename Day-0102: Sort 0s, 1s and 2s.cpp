a46c46aa6230e36133915a898339b47bd62a341f
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-09-09 21:13:16 +0530

    Create Day-0102: Sort 0s, 1s and 2s.cpp

---
+++ Day-0102: Sort 0s, 1s and 2s.cpp
@@ -0,0 +1,9 @@
+// Sort 0s, 1s and 2s (EASY)
+// https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+    }
+};
