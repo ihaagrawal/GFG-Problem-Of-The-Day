6705e24aa4ac95737ad67240957288a14eda639e
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-09-17 13:48:10 +0530

    Create Day-0110: Minimize the Heights II.cpp

---
+++ Day-0110: Minimize the Heights II.cpp
@@ -0,0 +1,24 @@
+// Minimize the Heights II (MEDIUM)
+// https://www.geeksforgeeks.org/problems/minimize-the-heights3351/1
+
+class Solution {
+  public:
+    int getMinDiff(vector<int> &arr, int k) {
+       int n = arr.size();
+       sort(arr.begin(),arr.end());
+        
+        int diff=arr[n-1]-arr[0];
+        int maxi,mini;
+        
+        
+        for(int i=1;i<n;i++){
+            if(arr[i]-k<0)continue;
+            
+            maxi=max(arr[i-1]+k,arr[n-1]-k);
+            mini=min(arr[0]+k,arr[i]-k);
+            
+            diff=min(diff,maxi-mini);
+        }
+       return diff;
+    }
+};
