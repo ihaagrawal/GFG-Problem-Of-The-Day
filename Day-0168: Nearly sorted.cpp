7c1c05497eda1909c499233df52f7e38f3a5abe1
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-11-14 11:30:37 +0530

    Create Day-0168: Nearly sorted.cpp

---
+++ Day-0168: Nearly sorted.cpp
@@ -0,0 +1,10 @@
+// Nearly sorted (MEDIUM)
+// https://www.geeksforgeeks.org/problems/nearly-sorted-1587115620/1
+
+
+class Solution {
+  public:
+    void nearlySorted(vector<int>& arr, int k) {
+        sort(arr.begin(), arr.end());
+    }
+};
