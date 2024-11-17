4bcc725eb9a12a3e7b5d9c565ca874fe49a23245
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-11-17 09:49:09 +0530

    Create Day-0171: Reverse an Array.cpp

---
+++ Day-0171: Reverse an Array.cpp
@@ -0,0 +1,10 @@
+// Reverse an Array (EASY)
+// https://www.geeksforgeeks.org/problems/reverse-an-array/1
+
+
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        reverse(arr.begin(), arr.end());
+    }
+};
