a446e241ca9b96f8dec5cad09a2c6dceec9fe3dd
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-11-19 21:51:04 +0530

    Create Day-0173: Next Permutation.cpp

---
+++ Day-0173: Next Permutation.cpp
@@ -0,0 +1,10 @@
+// Next Permutation (MEDIUM)
+// https://www.geeksforgeeks.org/problems/next-permutation5226/1
+
+
+class Solution {
+  public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(), nums.end());
+    }
+};
