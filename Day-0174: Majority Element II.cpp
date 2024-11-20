bcc936ecb2d9a311f84ac5725571148e8725f40e
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2024-11-20 12:32:37 +0530

    Create Day-0174: Majority Element II.cpp

---
+++ Day-0174: Majority Element II.cpp
@@ -0,0 +1,45 @@
+// Majority Element II (MEDIUM)
+// https://www.geeksforgeeks.org/problems/majority-vote/1
+
+
+class Solution {
+  public:
+    // Function to find the majority elements in the array
+    vector<int> findMajority(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return {};
+    
+        int candidate1 = -1, candidate2 = -1, count1 = 0, count2 = 0;
+    
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+    
+        count1 = count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+    
+        vector<int> result;
+        if (count1 > n / 3) result.push_back(candidate1);
+        if (count2 > n / 3) result.push_back(candidate2);
+    
+        if (result.empty()) return {};
+        sort(result.begin(), result.end());
+        return result;
+    }
+};
