c8c7b8e92f9f4c7ae91e451a3f12d9cb3b2e0b44
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2025-01-17 11:37:39 +0530

    Create Day-0232: Product array puzzle.cpp

---
+++ Day-0232: Product array puzzle.cpp
@@ -0,0 +1,25 @@
+// Product array puzzle (EASY)
+// https://www.geeksforgeeks.org/problems/product-array-puzzle4525/1
+
+
+class Solution {
+  public:
+    vector<int> productExceptSelf(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> res(n, 1);
+        
+        int prefix = 1;
+        for (int i = 0; i < n; ++i) {
+            res[i] = prefix;
+            prefix *= arr[i];
+        }
+        
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            res[i] *= suffix;
+            suffix *= arr[i];
+        }
+        
+        return res;
+    }
+};
