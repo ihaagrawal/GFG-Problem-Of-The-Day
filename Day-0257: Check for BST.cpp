785ccf3be21bb1e394f8eb792d954cab7d16598f
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2025-02-11 14:15:15 +0530

    Create Day-0257: Check for BST.cpp

---
+++ Day-0257: Check for BST.cpp
@@ -0,0 +1,26 @@
+// Check for BST (EASY)
+// https://www.geeksforgeeks.org/problems/check-for-bst/1
+
+
+class Solution {
+  public:
+    // Function to check whether a Binary Tree is BST or not.
+    void check(Node* &root, vector<int> &arr){
+        if(root==NULL) return;
+        
+        check(root->left,arr);
+        arr.push_back(root->data);
+        check(root->right,arr);
+    }
+    
+    bool isBST(Node* root) {
+        vector<int> arr;
+        check(root,arr);
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<=arr[i-1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
