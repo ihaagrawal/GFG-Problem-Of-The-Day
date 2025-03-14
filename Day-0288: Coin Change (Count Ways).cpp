98f32354449a27c60f354a0163771d44db812148
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2025-03-14 18:57:58 +0530

    Create Day-0288: Coin Change (Count Ways).cpp

---
+++ Day-0288: Coin Change (Count Ways).cpp
@@ -0,0 +1,18 @@
+// Coin Change (Count Ways) (MEDIUM)
+// https://www.geeksforgeeks.org/problems/coin-change2448/1
+
+class Solution {
+  public:
+    int count(vector<int>& coins, int sum) {
+        vector<int> dp(sum + 1, 0);
+        dp[0] = 1;
+    
+        for (int coin : coins) {
+            for (int j = coin; j <= sum; j++) {
+                dp[j] += dp[j - coin];
+            }
+        }
+    
+        return dp[sum];
+    }
+};
