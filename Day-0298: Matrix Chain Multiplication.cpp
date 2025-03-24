3bb2346703c100abb2070d1247c49c20833e4cbd
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2025-03-24 13:54:27 +0530

    Create Day-0298: Matrix Chain Multiplication.cpp

---
+++ Day-0298: Matrix Chain Multiplication.cpp
@@ -0,0 +1,24 @@
+// Matrix Chain Multiplication (HARD)
+// https://www.geeksforgeeks.org/problems/matrix-chain-multiplication0303/1
+
+class Solution {
+  public:
+    int matrixMultiplication(vector<int> &arr) {
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        for (int length = 2; length < n; length++) {
+            for (int i = 1; i < n - length + 1; i++) {
+                int j = i + length - 1;
+                dp[i][j] = INT_MAX;
+                for (int k = i; k < j; k++) {
+                    int cost = dp[i][k] + dp[k+1][j] + arr[i-1] * arr[k] * arr[j];
+                    dp[i][j] = min(dp[i][j], cost);
+                }
+            }
+        }
+        
+        return dp[1][n-1];
+        
+    }
+};
