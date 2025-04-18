3226303e8c25d004335e744c497030095080e0bd
Iha Agrawal <132403976+ihaagrawal@users.noreply.github.com>
2025-04-18 10:58:30 +0530

    Create Day-0323: Implement Trie.cpp

---
+++ Day-0323: Implement Trie.cpp
@@ -0,0 +1,55 @@
+// Implement Trie (MEDIUM)
+// https://www.geeksforgeeks.org/problems/trie-insert-and-search0651/1
+
+class TrieNode {
+public:
+    unordered_map<char, TrieNode*> children;
+    bool isEndOfWord;
+
+    TrieNode() {
+        isEndOfWord = false;
+    }
+};
+
+class Trie {
+private:
+    TrieNode* root;
+
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(string &word) {
+        TrieNode* current = root;
+        for (char c : word) {
+            if (current->children.find(c) == current->children.end()) {
+                current->children[c] = new TrieNode();
+            }
+            current = current->children[c];
+        }
+        current->isEndOfWord = true;
+    }
+
+    bool search(string &word) {
+        TrieNode* current = root;
+        for (char c : word) {
+            if (current->children.find(c) == current->children.end()) {
+                return false;
+            }
+            current = current->children[c];
+        }
+        return current->isEndOfWord;
+    }
+
+    bool isPrefix(string &word) {
+        TrieNode* current = root;
+        for (char c : word) {
+            if (current->children.find(c) == current->children.end()) {
+                return false;
+            }
+            current = current->children[c];
+        }
+        return true;
+    }
+};
